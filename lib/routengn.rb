51d460faf9a82de362a773a2c1bee702ae781dde
Quin Hoxie <qhoxie@gmail.com>
2009-09-18 00:42:52 -0700

    Refactor how the get method works.  Add response class to ease working with api responses.

--- lib/routengn.rb
+++ lib/routengn.rb
@@ -45,11 +45,19 @@ module RouteNGN
       @connection
     end
 
-    [:get, :post, :put, :delete].each do |http_method|
-      define_method(http_method) do |uri|
+    def get(uri, params = {})
+      url = params.inject("#{uri}?") { |_, (k, v)| _ << "#{k}=#{v}" }
+      raw = @connection.access_token.get url
+      Response.new raw
+    end
+
+    [:post, :put, :delete].each do |http_method|
+      define_method(http_method) do |*args|
+        uri, params = args
+        params ||= {}
         check_connection!
-        raw = @connection.access_token.send http_method, uri
-        JSON.parse raw.body
+        raw = @connection.access_token.send http_method, uri, params
+        Response.new raw
       end
     end
 
@@ -57,6 +65,29 @@ module RouteNGN
     def check_connection!
       raise ConnectionException.new unless @connection
       raise OAuthException.new unless @connection.access_token
+    end
+  end
+
+  class Response
+    SUCCESS_RANGE = (200..399)
+
+    attr_reader :data, :status
+
+    def initialize(raw)
+      @status = raw.code
+      @data = JSON.parse raw.body
+    end
+
+    def [](key)
+      @data[key]
+    end
+
+    def success?
+      SUCCESS_RANGE.include? @status
+    end
+
+    def failure?
+      !success?
     end
   end
 
