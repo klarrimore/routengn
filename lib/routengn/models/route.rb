73cb3702b26c18b10a25c6f24d1f1b5807673543
klarrimore <klarrimore@lesnar.(none)>
2009-10-06 15:03:13 -0400

    added epg_block and epg_order to route

--- lib/routengn/models/route.rb
+++ lib/routengn/models/route.rb
@@ -2,7 +2,10 @@ class Route
   include RouteNGN::Mapper
 
   field :id, :primary => true
+  field :epg_block
+  field :epg_order
   
   belongs_to :instance
+  belongs_to :locale
   belongs_to :type
 end
